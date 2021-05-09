84beb9802bd80168dc45953798c8501755526a11
Kruti Patel <kruti.patel0709@gmail.com>
2021-05-09 06:53:34 -0400

    add some script into .js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,49 @@
 // Assignment code here
 
-function criteria(){
+function criteria() {
   var upperCase = confirm('Do you want to add UPPER CASE characters?');
   var lowerCase = confirm('Do you want to add LOWER CASE characters?');
   var special = confirm('Do you want to add SPECIAL characters?');
   var numeric = confirm('Do you want to add NUMERIC characters?');
 }
 
-function generatePassword(){
-  var letterLenghth = window.prompt('How many characters would you like your password to contain?');
-  console.log(letterLenghth);
+function generatePassword() {
+  var letterLenghth = window.prompt(
+    'How many characters would you like your password to contain?'
+  );
+  console.log(typeof letterLenghth);
+  if (letterLenghth > 256 || letterLenghth < 8) {
+    alert('Password must be less than 258 characters.');
+    return generatePassword();
+  }
   var upperCase = confirm('Do you want to add UPPER CASE characters?');
   var lowerCase = confirm('Do you want to add LOWER CASE characters?');
   var special = confirm('Do you want to add SPECIAL characters?');
   var numeric = confirm('Do you want to add NUMERIC characters?');
 
-  if (letterLenghth > 256) {  
-    alert("Password must be less than 258 characters.")
+  if (
+    upperCase == false &&
+    lowerCase == false &&
+    special == false &&
+    numeric == false
+  ) {
+    alert('You must have to select atleast one type.');
+    return generatePassword();
   }
-  else if(letterLenghth < 8){
-    alert("PAssword must be atleast 8 characters.")
-  }
-  else if(upperCase == false && lowerCase == false && special == false && numeric == false){
-    alert("You must have to select atleast one type.");
-  }
-  else{
-    return 0;
-  }
+
+  var userCriteria = {
+    pwLength: parseInt(letterLenghth),
+    upperCase: upperCase,
+    lowerCase: lowerCase,
+    special: special,
+    numeric: numeric,
+  };
+
+  console.log('This is the password length: ', userCriteria.pwLength);
+  console.log('User wants upper case char? ', userCriteria.upperCase);
+  console.log('User wants lower case char? ', userCriteria.lowerCase);
+  console.log('User wants special char? ', userCriteria.special);
+  console.log('User wants numeric char? ', userCriteria.numeric);
 
   //   if (upperCase == true && lowerCase == true && special == true && numeric == true){
   //     console.log(upperCase, lowerCase, special,numeric);
@@ -36,43 +53,37 @@ function generatePassword(){
   //   }
   // }
 
-
   // if (upperCase == false && lowerCase == false && special == false && numeric == false){
   //   alert("You must have to select atleast one type.");
   //   generatePassword();
   // }
 
+  //   if (letterLenghth === true){
+  //     generatePassword();
+  //   }
+  //   else{
+  //     alert("Password must be 8 characters.")
+  //   }
 
-
-//   if (letterLenghth === true){
-//     generatePassword();
-//   }
-//   else{
-//     alert("Password must be 8 characters.")
-//   }
-  
-//   var char = '';
-//   for (i = 0; i < 8; i++){
-//     char = Math.floor(Math.random() * letterLenghth.length);
-//     console.log(char);
-//   }
-//   return char;
-
+  //   var char = '';
+  //   for (i = 0; i < 8; i++){
+  //     char = Math.floor(Math.random() * letterLenghth.length);
+  //     console.log(char);
+  //   }
+  //   return char;
 }
-generatePassword();
 // criteria();
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
