aeb28fcfec93c4a4902ea627df6b020aeb81764b
Kruti Patel <kruti.patel0709@gmail.com>
2021-05-09 22:58:43 -0400

    add character types:lowercase, uppercase, numeric, special characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,13 @@
 //function of password generator
 function generatePassword() {
 
+  let upperCharSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
+  "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let lowerCharSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
+    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let specialCharSet = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":"];
+  let numberCharSet = [1,2,3,4,5,6,7,8,9,10];
+
   var letterLength = window.prompt(
     'How many characters would you like your password to contain?'
   ); 
